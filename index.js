cd440cc404d11a8fc43fff3a4b8bcf05cbb37c9e
Prajjwal Goswami <>
2022-12-21 18:44:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/*var readlineSync = require('readline-sync');
+//input
+var userAnswerAge = readlineSync.question("Am I older than 25");
+//processing
+if (userAnswerAge === "yes") {
+  console.log("You are right")
+}
+else {
+  //output
+  console.log("You are wrong")
+}
+
+*/
+
+
+var welcome = "Welcome Prajjwal";
+console.log(welcome);
